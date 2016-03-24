7cc7fc12277ddf38c88e6de5a60b42beea4f9614
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-23 20:26:27 -0400

    Adjusting spacing and bracketing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var $input = $('#to-do-input');
 var $list = $('.list');
 
-$('form').on('submit', function (e){
+$('form').on('submit', function (e) {
   var $li = $('<li>');
 
   e.preventDefault();
-  $li.html($input.val());
+  $li.html($input.val(''));
   $list.append($li);
-  $input.val(' ');
+  $input.val('');
 });
 
-$('li').on('click', 'li', function (e){
+$('li').on('click', 'li', function (e) {
   $(this).toggleClass('line');
 });
